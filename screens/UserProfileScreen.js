da0642f82b5d6e5b5333e8397988da363c07a85b
ConnectionsLLC <0aniketmishra@gmail.com>
2023-07-29 09:38:47 +0000

    Added Follow feature

--- screens/UserProfileScreen.js
+++ screens/UserProfileScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, Image, Pressable, TouchableOpacity, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
 import Header from '../components/Header'
 import Stories from '../components/Stories'
@@ -7,6 +7,7 @@ import Post from '../components/Post'
 import { Entypo } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios'
 
 
 const UserProfileScreen = () => {  
@@ -16,7 +17,39 @@ const UserProfileScreen = () => {
   const [currentTab, setCurrentTab] = useState("Posts") 
   const [posts, setPosts] = useState({ "__v": 0, "_id": "649d0c31f332ee28bf2c19c4", "comments": [], "email": "a@gmail.com", "image1": "https://firebasestorage.googleapis.com/v0/b/social-368115.appspot.com/o/Bb?alt=media&token=8b2dce78-7b80-47a2-b098-9786cbf95082", "image2": "https://firebasestorage.googleapis.com/v0/b/social-368115.appspot.com/o/Bb2?alt=media&token=5a21f371-3fc0-43dc-bf9f-4473558ff5f8", "image3": "", "image4": "", "likes": [], "posttext": "A random picture of a project.. " })
   const navigation = useNavigation()
+  const [isFollowing,setIsFollowing] = useState(false)
 
+  const handleFollow = async () => {
+   
+    setIsFollowing(true)
+    userdata.followers.push(user.email)
+    fetch('https://social-backend-three.vercel.app/followuser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      followfrom: user.email,
+      followto: userdata.email
+    })
+  })
+  
+}
+const handleUnfollow = () => {
+  setIsFollowing(false)
+  userdata.followers.pop(user.email)
+  fetch('https://social-backend-three.vercel.app/unfollowuser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      unfollowfrom: user.email,
+      unfollowto: userdata.email
+    })
+  })
+   
+}
   return (
     <View style={{ flex: 1, backgroundColor: 'white' }}>
       <ScrollView>
@@ -27,7 +60,7 @@ const UserProfileScreen = () => {
             <Ionicons name="arrow-back" size={30} color="black" />
             </TouchableOpacity>
           </View>
-         
+          
          
          <View style={{ marginLeft: 6, flex: 1, flexDirection: 'row', alignItems: 'center' }}>
             <Text style={{ fontSize: 18, fontWeight: "bold", }}>{userdata?.username}</Text>
@@ -77,9 +110,17 @@ const UserProfileScreen = () => {
               <Text style={{ fontWeight: 'bold',  fontSize: 14, }}>Edit Profile</Text>
             </Pressable>
           ) : (
-            <Pressable titleSize={20} style={{ backgroundColor: '#F5F5F5', justifyContent: 'center', alignItems: 'center', borderRadius: 8, marginRight: 5, flex: 1, height: 34 }}>
-            <Text style={{ fontWeight: 'bold',  fontSize: 14, }}>Follow</Text>
+           <>
+           {isFollowing == true || userdata.followers.includes(user.email) ? (
+ <Pressable onPress={handleUnfollow} titleSize={20} style={{ backgroundColor: '#F5F5F5', justifyContent: 'center', alignItems: 'center', borderRadius: 8, marginRight: 5, flex: 1, height: 34 }}>
+ <Text style={{ fontWeight: 'bold',  fontSize: 14, }}>Following</Text>
+</Pressable>
+           ) : (
+            <Pressable onPress={handleFollow} titleSize={20} style={{ backgroundColor: '#0076E5', justifyContent: 'center', alignItems: 'center', borderRadius: 8, marginRight: 5, flex: 1, height: 34 }}>
+            <Text style={{ fontWeight: 'bold',  fontSize: 14,color: 'white' }}>Follow</Text>
           </Pressable>
+           )}
+           </>
           )}
             <Pressable titleSize={20} style={{ backgroundColor: '#F5F5F5', justifyContent: 'center', alignItems: 'center', borderRadius: 8, marginLeft: 5, flex: 1, height: 34 }}>
               <Text style={{ fontWeight: 'bold', fontSize: 14, }}>Share Profile</Text>
